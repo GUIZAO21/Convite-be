bb96ee905daf9018d6d7e0df3bf3424da4f9c9e4
GUIZAO21 <guiguifolha3@gmail.com>
2024-12-27 14:00:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function moveButton() {
-    var noButton = document.getElementById("noButton");
-    
-    // Movimento aleatório do botão
-    var maxX = window.innerWidth - noButton.offsetWidth;
-    var maxY = window.innerHeight - noButton.offsetHeight;
-    
-    var randomX = Math.floor(Math.random() * maxX);
-    var randomY = Math.floor(Math.random() * maxY);
-    
-    noButton.style.position = "absolute";
-    noButton.style.left = randomX + "px";
-    noButton.style.top = randomY + "px";
-}
+document.getElementById('confirmButton').addEventListener('click', function() {
+  alert('Sua presença foi confirmada! Estamos aguardando por você.');
+});
